023ec2d2666d34598320ad1faa995ba824e4ce7f
Gustavo Alencar <gustavorique@live.com>
2023-09-19 01:50:08 -0300

    feat: validação de cliente criada

--- src/main/java/com/clube/smartphone/controllers/ClienteController.java
+++ src/main/java/com/clube/smartphone/controllers/ClienteController.java
@@ -5,12 +5,17 @@ import com.clube.smartphone.repositories.EnderecoRepository;
 import com.clube.smartphone.services.AparelhoService;
 import com.clube.smartphone.services.ClienteService;
 import com.clube.smartphone.services.EnderecoSerivce;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/clientes")
@@ -32,10 +37,35 @@ public class ClienteController {
     }
 
     @PostMapping("/cadastrar")
-    public ResponseEntity<Cliente> salvar(@RequestBody Cliente cliente, BindingResult result) {
-        serviceEndereco.save(cliente.getEndereco());
-        serviceAparelho.save(cliente.getAparelho());
-        return ResponseEntity.status(HttpStatus.CREATED).body(serviceCliente.save(cliente));
+    public ResponseEntity<Object> salvar(@RequestBody @Valid Cliente cliente, BindingResult result) {
+
+        if (result.hasErrors()) {
+            List<String> errors = new ArrayList<>();
+            for (FieldError error : result.getFieldErrors()) {
+                errors.add(error.getDefaultMessage());
+            }
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("success", false);
+            response.put("message", "Erro de validação");
+            response.put("errors", errors);
+
+            return ResponseEntity.badRequest().body(response);
+
+        } else {
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("success", true);
+            response.put("message", "Cliente cadastrado com sucesso");
+
+            serviceEndereco.save(cliente.getEndereco());
+            serviceAparelho.save(cliente.getAparelho());
+            serviceCliente.save(cliente);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(cliente);
+
+        }
+
     }
 
 }
