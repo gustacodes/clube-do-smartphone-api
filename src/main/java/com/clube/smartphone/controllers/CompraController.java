7a270d7a3fe8e230981b5099d67c622803192d8f
Gustavo Alencar <gustavorique@live.com>
2023-10-05 00:57:16 -0300

    feat: módulo financeiro concluído

--- src/main/java/com/clube/smartphone/controllers/CompraController.java
+++ src/main/java/com/clube/smartphone/controllers/CompraController.java
@@ -1,13 +1,12 @@
 package com.clube.smartphone.controllers;
 
 import com.clube.smartphone.entities.Compra;
-import com.clube.smartphone.entities.Financeiro;
-import com.clube.smartphone.entities.Produtos;
 import com.clube.smartphone.enums.Pagamento;
 import com.clube.smartphone.services.CompraService;
-import com.clube.smartphone.services.ProdutosServices;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/comprar")
@@ -19,8 +18,13 @@ public class CompraController {
         this.compraService = compraService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<Compra>> listar() {
+        return ResponseEntity.ok().body(compraService.listar());
+    }
+
     @PutMapping("/{id}/{quantidade}/{pagamento}")
-    public ResponseEntity<Compra> compra(@PathVariable Long id, @PathVariable Integer quantidade, @PathVariable Pagamento pagamento) {
+    public ResponseEntity<Compra> compra(@PathVariable Long id, @PathVariable Double quantidade, @PathVariable Pagamento pagamento) {
 
         Compra compra = compraService.compra(id, quantidade, pagamento);
         return ResponseEntity.ok().body(compra);
